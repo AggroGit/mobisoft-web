d593dce58494b2f9bcef6ef2a1b9f73c25ae691d
poropo97 <poropo97@gmail.com>
2021-01-25 18:19:29 +0100

    ds

--- resources/views/layouts/city.blade.php
+++ resources/views/layouts/city.blade.php
@@ -5,9 +5,9 @@
   <title>@yield('headTitle')</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" async rel="preload" as="style" onload="this.rel='stylesheets'">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" async rel="preload" as="style" onload="this.rel='stylesheets'"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" async rel="preload" as="style" onload="this.rel='stylesheets'"></script>
   <!-- Letra Neon -->
   <!-- <link href='https://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'> -->
   <!-- Carga delayed de imagenes -->

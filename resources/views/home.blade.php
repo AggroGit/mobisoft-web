38b5ef158da47bdfbe4a5cda1be36ae1875f2706
poropo97 <poropo97@gmail.com>
2021-01-30 16:15:33 +0100

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,5 +28,26 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 
+    <div class="content">
+      <div class="contieneLema">
+        <div class="lema">
+          <p>Nuestro lema en <span>Mobility</span> </p>
+        </div>
+        <q cite="">Cuando la confianza es alta, la comunicación es fácil, instantánea y efectiva</q>
+      </div>
+      <div class="triangle"></div>
+      <div class="citado">
+        <div class="citaimg">
+          <img src="/imagenes/Stephen.jpg" alt="Stephen R. Covey">
+        </div>
+        <div class="quienCita">
+          <span>Stephen R. Covey</span>
+          <p>Escritor y conferenciante</p>
+        </div>
+      </div>
+
+
+    </div>
+
   </body>
 </html>

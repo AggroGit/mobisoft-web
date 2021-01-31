f485ced01ee4c5d49f259ce42d8f031eba51e2e4
poropo97 <poropo97@gmail.com>
2021-02-01 00:31:33 +0100

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,9 +29,6 @@
       </a>
     </div>
 
-    <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
 
     <div class="content">
       <h2>Quienes somos</h2>
@@ -87,9 +84,19 @@
           <a class="borde-linear | boton-sencillo | down-40" href="">Ver Portfolio</a>
         </div>
       </div>
-
-
     </div>
+
+
+    <div class="content | slideanim">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+
+    <footer>
+      <div class="content">
+        <p>Made with ❤️ by <span>Mobisoft</span> </p>
+      </div>
+    </footer>
 
 
 

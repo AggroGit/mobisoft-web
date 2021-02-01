5c1c9aa5ddc4a56ae3e464a3417e383bb47341ee
poropo97 <poropo97@gmail.com>
2021-02-01 01:04:48 +0100

    navbar

---
+++ resources/views/components/cabezera.blade.php
@@ -0,0 +1,8 @@
+<nav class="borde-linear">
+  <div class="icon">
+    <img src="{{asset('logos/mobisoft-mini.png')}}" alt="Mobility Software Solutions">
+  </div>
+  <div class="contact | neonhover">
+    <a href="#">Contacta</a>
+  </div>
+</nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,12 +14,10 @@
   </head>
   <body>
     @yield('content')
+    <x-cabezera/>
 
-    <nav>
-      <div class="icon">
-        <img src="{{asset('logos/mobisoft-mini.png')}}" alt="Mobility Software Solutions">
-      </div>
-    </nav>
+
+
     <div class="content | welcome | slideanim">
       <h1>Mobisoft | Empresa de desarrollo de apps, software y webs a medida</h1>
       <span class="entrelineado | linea-lila">Somos verdaderos expertos en crear proyectos que funcionan tanto para ti como tus usuarios.</span>
@@ -87,9 +85,7 @@
     </div>
 
 
-    <div class="content | slideanim">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
+
 
 
     <footer>

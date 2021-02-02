a4256eb555e3567a18fda9c34b1a51d30093478c
poropo97 <poropo97@gmail.com>
2021-02-02 13:29:52 +0100

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,32 +84,55 @@
       </div>
     </div>
 
-    <div class="content-big | slideanim">
-      <div class="divided">
-        <div class="divided-1-2">
-          <h2>Conceptualización de la idea</h2>
-          <p>Iniciamos el proyecto realizando un "brainstorming" que consiste en reflejar todas las ideas clave sobre las funcionalidades.</p>
-          <p>Seguimos ordenando estas ideas antes de empezar el desarrollo de apps en Barcelona gracias al "card sorting”</p>
-        </div>
-        <div class="divided-1-2">
-          <img class="unselectable | " src="/desarrollo/idea-hacer-app.png" alt="">
-        </div>
+    <div class="" id="proceso">
+      <div class="content-big | slideanim">
+        <div class="divided">
+          <div class="divided-1-2">
+            <h2>Conceptualización de la idea</h2>
+            <p>Iniciamos el proyecto realizando un "brainstorming" que consiste en reflejar todas las ideas clave sobre las funcionalidades.</p>
+            <p>Seguimos ordenando estas ideas antes de empezar el desarrollo de apps en Barcelona gracias al "card sorting”</p>
+          </div>
+          <div class="divided-1-2">
+            <img class="unselectable | " src="/desarrollo/idea-hacer-app.png" alt="">
 
+          </div>
+
+        </div>
       </div>
+
+
+      <div class="content-big | slideanim">
+        <div class="divided">
+          <div class="divided-1-2 ">
+            <img class="unselectable | " src="/desarrollo/interfaz-app.png" alt="">
+          </div>
+          <div class="divided-1-2">
+            <h2>Interfaz y experiencia de usuario</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="content-big | slideanim">
+        <div class="divided">
+          <div class="divided-1-2">
+            <h2>Programación y publicación</h2>
+            <p>Iniciamos el proyecto realizando un "brainstorming" que consiste en reflejar todas las ideas clave sobre las funcionalidades.</p>
+            <p>Seguimos ordenando estas ideas antes de empezar el desarrollo de apps en Barcelona gracias al "card sorting”</p>
+          </div>
+          <div class="divided-1-2">
+            <img class="unselectable | " src="/desarrollo/publicar-tu-app.png" alt="">
+          </div>
+
+        </div>
+      </div>
+
+
+
     </div>
 
 
-    <div class="content-big | slideanim">
-      <div class="divided">
-        <div class="divided-1-2 ">
-          <img class="unselectable | " src="/desarrollo/idea-hacer-app.png" alt="">
-        </div>
-        <div class="divided-1-2">
-          <h2>Interfaz y experiencia de usuario</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-    </div>
 
 
 

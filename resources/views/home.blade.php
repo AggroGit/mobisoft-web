2e7f78c208e1ab69ccdcc921afad84f4de8552cd
poropo97 <poropo97@gmail.com>
2021-02-02 13:05:58 +0100

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,11 +75,38 @@
 
     <div class="content slideanim" id="test">
       <div class="divided">
-        <img class="divided-1-2 | img-rot" src="/imagenes/apps/collblanc.png" alt="">
+        <img class="divided-1-2 |  | unselectable" src="/imagenes/apps/collblanc.png" alt="">
         <div class="divided-1-2 | inlined">
           <h2>Lorem ipsum</h2>
           <p>These opinions paint a picture of a language getting more mature, but also more complex. And perhaps –at least while we struggle to keep up with the pace of change– a little less enjoyable?</p>
           <a class="borde-linear | boton-sencillo | down-40" href="">Ver Portfolio</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="content-big | slideanim">
+      <div class="divided">
+        <div class="divided-1-2">
+          <h2>Conceptualización de la idea</h2>
+          <p>Iniciamos el proyecto realizando un "brainstorming" que consiste en reflejar todas las ideas clave sobre las funcionalidades.</p>
+          <p>Seguimos ordenando estas ideas antes de empezar el desarrollo de apps en Barcelona gracias al "card sorting”</p>
+        </div>
+        <div class="divided-1-2">
+          <img class="unselectable | " src="/desarrollo/idea-hacer-app.png" alt="">
+        </div>
+
+      </div>
+    </div>
+
+
+    <div class="content-big | slideanim">
+      <div class="divided">
+        <div class="divided-1-2 ">
+          <img class="unselectable | " src="/desarrollo/idea-hacer-app.png" alt="">
+        </div>
+        <div class="divided-1-2">
+          <h2>Interfaz y experiencia de usuario</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
     </div>
@@ -136,9 +163,7 @@
         if(document.documentElement.scrollTop+600 >= elements[i].offsetTop || elements[i].offsetTop<=100){
           elements[i].classList.add('slide')
         }
-
       }
-
     }
 
 

9a914d7dc19e7b8953207382d539349b87f59f5f
poropo97 <poropo97@gmail.com>
2021-02-03 11:52:11 +0100

    android

--- resources/views/components/cabezera.blade.php
+++ resources/views/components/cabezera.blade.php
@@ -1,9 +1,39 @@
-<nav class="borde-linear">
+<nav class="borde-linear | fixed">
   <div class="hamb-menu" id="nav-icon3">
     <span></span>
     <span></span>
     <span></span>
     <span></span>
+  </div>
+  <div class="menu menu-open">
+    <ul class="listado-menu">
+      <li> <a href="#">Desarrollo Apps</a> </li>
+      <li> <a href="#">Desarrollo Web</a> </li>
+      <li> <a href="#">Software</a> </li>
+      <li> <a href="#">Portfolio</a> </li>
+      <li>
+        <a href="#">dESPLEGABLE</a>
+        <ul>
+          <li> <a href="#">Opcion</a> </li>
+          <li> <a href="#">Opcion</a> </li>
+        </ul>
+      </li>
+
+    </ul>
+    <div class="bocata-help | borde-linear">
+      <h5>¿Necesitas <span>ayuda</span> ?</h5>
+      <div class="">
+        <img class="phone-menu" src="/imagenes/phone.png" alt="">
+        <span>Te llamamos gratis</span>
+      </div>
+      <div class="">
+        <img src="/imagenes/whatsapp.png" alt="">
+        <span>Whatsapp</span>
+      </div>
+      <div class="android-helper">
+        <img src="/imagenes/android.png" alt="">
+      </div>
+    </div>
   </div>
   <div class="icon">
     <a href="/">

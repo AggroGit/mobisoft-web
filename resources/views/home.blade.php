9f1f49f97b867a716c7b079e356e16ded304b273
poropo97 <poropo97@gmail.com>
2021-02-04 14:22:22 +0100

    coming the services

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,28 +17,38 @@
     </div>
   </section>
 
-
-  <x-nosotros/>
-
-  <section class="content">
-    <div class="borde-linear">
-      <h2 class="collapsible">Empresa desarrollo de software</h2>
-      <div class="contentCollapsable">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <section class="content-big | slideanim" id="services">
+    <div class="divided | servicesContent">
+      <div class="divided-1-3">
+        <div class="svg | web-svg">
+          <x-svg.web/>
+        </div>
+        <h3>Páginas Web y Aplicaciones Web</h3>
+      </div>
+      <div class="divided-1-3">
+        <div class="svg | web-svg">
+          <x-svg.web/>
+        </div>
+        <h3>Apps para Android y iPhone</h3>
+      </div>
+      <div class="divided-1-3">
+        <div class="svg | web-svg">
+          <x-svg.web/>
+        </div>
+        <h3>Marketing y asesoramiento digital</h3>
       </div>
     </div>
   </section>
 
 
-      <div class="content">
-        <div class="borde-linear">
-          <h2 class="collapsible">Empresa desarrollo de software</h2>
-          <div class="contentCollapsable">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          </div>
-        </div>
-      </div>
+
+
+
+
+
+
+
+
 
 
       <div class="content slideanim" id="test">

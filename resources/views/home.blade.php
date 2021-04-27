f082f6063ac01544587896c804d16d0c94fa5383
poropo97 <Ganimedes97>
2021-04-27 14:41:27 +0200

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 <div class="cabezera">
   <section class="content-big">
     <div class="divided | noPadding | reverseOnPhone">
-      <div class="divided-1-2  | titleSubtitle">
+      <div class="divided-1-2  | titleSubtitle | noTopMob">
         <h1>Mobisoft | Empresa de Desarrollo de Apps, Software y Webs a Medida</h1>
         <p>Soluciones innovadoras para usuarios reales, nuestro equipo de desarrolladores interno es experto en aplicaciones móviles y lanzamiento de Startups</p>
         <p>Operamos con total normalidad a pesar de la situación actual. ¿Buscas presupuesto gratuito? Te ayudamos.</p>

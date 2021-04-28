d608a2a082dde6118289b2b870c9d257a908b4ea
poropo97 <Ganimedes97>
2021-04-28 15:07:21 +0200

    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
 
       </div>
       <div class="divided-1-2 | flex | imgCabezera | unselectable" >
-        <img class="unselectable | " data-src="/imagenes/apps/both.png " alt="Aplicacion Merco para Comercios">
+        <img data-src="/imagenes/apps/both.png" class="unselectable"  alt="Aplicacion Merco para Comercios">
       </div>
     </div>
   </section>

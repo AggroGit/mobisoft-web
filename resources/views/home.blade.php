4956ffe99f4e18e5d68efe1bd65c49abbd430078
poropo97 <Ganimedes97>
2021-07-11 23:14:27 +0200

    quito comentario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
 
 @section('head')
 <meta name="description" content="Empresa de Desarrollo de Apps, Software y Webs a Medida para impulsar tu negocio o idea."/>
-  <!-- Hola, esto es una prueba -->
 @endsection
 
 @section('content')
